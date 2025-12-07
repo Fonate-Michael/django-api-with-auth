b7c725ad5b9bf858978b28151bdc85e5265c1ccf
Fonate-Michael <developerfonate@gmail.com>
2025-12-08 00:27:16 +0100

    added the some user restrictions

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import BasePermission, IsAuthenticated, SAFE_METHODS
+
 from .models import Post
 from .serializers import PostSerializers
-from rest_framework.permissions import IsAuthenticated
+
+
+class IsAuthorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+      
+        if request.method in SAFE_METHODS:
+            return True
+
+       
+        return obj.author == request.user
 
 
 class PostViewList(generics.ListCreateAPIView):
@@ -21,7 +32,7 @@ class PostViewCreate(generics.CreateAPIView):
 
 
 class PostViewUpdateDeleteRetrive(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
     serializer_class = PostSerializers
     queryset = Post.objects.all()
     lookup_field = 'pk'
